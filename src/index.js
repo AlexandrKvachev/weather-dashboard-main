8899688891e6c06ffdfa8d4b1c240ad2c86261ec
AlexandrKvachev <karkadan47@mail.ru>
2025-12-16 09:05:06 +0300

    Added correct display of temperature and weather images

--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import { getWeather} from "./handlers.js"
 const main = () => {
     const btn = document.querySelector(".fetch-button")
-    console.log(btn)
     btn.addEventListener("click", async () => {
         try {
-        const res = getWeather()
+        const res = await getWeather()
+        console.log(res)
         const content = document.querySelector(".content")
-        content.textContent = "Current temperature is " + res.main.temp  
+        const weatherIcon = document.querySelector(".weather-icon")
+        const weather = res.weather[0].main.toLowerCase()
+        const temp = Math.round(res.main.temp)
+        content.textContent = `Current temperature is ${temp}°C, ` + `and the weather is ${weather}`
+
+        if (weather === "clouds") {
+            weatherIcon.src = "./assets/clouds.jpg"
+        } else if (weather === "rain") {
+            weatherIcon.src = "./assets/rain.jpg"
+        } else if (weather === "snow") {
+            weatherIcon.src = "./assets/snow.jpg"
+        } else if (weather === "clear") {
+            weatherIcon.src = "./assets/clear.jpg"
+        } else if (weather === "atmosphere") {
+            weatherIcon.src = "./assets/fog.jpg"
+        } else if (weather === "drizzle") {
+            weatherIcon.src = "./assets/drizzle.jpg"
+        } else {
+            weatherIcon.src = "./assets/thunder.jpg"
+        }
         } catch(e) {
             alert("error")
+            console.log(e)
         }
     })
 }
