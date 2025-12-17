7b437fddb1ec50a22c50712d085e26b5b97e3b23
AlexandrKvachev <karkadan47@mail.ru>
2025-12-17 09:38:08 +0300

    added location string and replaced weather images

--- src/index.js
+++ src/index.js
@@ -5,26 +5,33 @@ const main = () => {
         try {
         const res = await getWeather()
         console.log(res)
+        const location = document.querySelector(".location")
         const content = document.querySelector(".content")
         const weatherIcon = document.querySelector(".weather-icon")
         const weather = res.weather[0].main.toLowerCase()
+        const curLocation = res.name
         const temp = Math.round(res.main.temp)
+        location.textContent = `${curLocation}`
         content.textContent = `Current temperature is ${temp}°C, ` + `and the weather is ${weather}`
 
         if (weather === "clouds") {
-            weatherIcon.src = "./assets/clouds.jpg"
+            weatherIcon.src = "./assets/clouds.gif"
+            weatherIcon.classList.add("weather-icon-clouds")
         } else if (weather === "rain") {
-            weatherIcon.src = "./assets/rain.jpg"
+            weatherIcon.src = "./assets/rain.gif"
         } else if (weather === "snow") {
-            weatherIcon.src = "./assets/snow.jpg"
+            weatherIcon.src = "./assets/snow.gif"
+            weatherIcon.classList.add("weather-icon-snow")
         } else if (weather === "clear") {
-            weatherIcon.src = "./assets/clear.jpg"
+            weatherIcon.src = "./assets/clear.gif"
+            weatherIcon.classList.add("weather-icon-clear")
         } else if (weather === "atmosphere") {
-            weatherIcon.src = "./assets/fog.jpg"
+            weatherIcon.src = "./assets/clouds.gif"
+            weatherIcon.classList.add("weather-icon-clouds")
         } else if (weather === "drizzle") {
-            weatherIcon.src = "./assets/drizzle.jpg"
+            weatherIcon.src = "./assets/rain.gif"
         } else {
-            weatherIcon.src = "./assets/thunder.jpg"
+            weatherIcon.src = "./assets/thunder.gif"
         }
         } catch(e) {
             alert("error")
